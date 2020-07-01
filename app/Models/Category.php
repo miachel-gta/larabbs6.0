628b21884a2fbdd24678ee2f83de4c085def6c71
wufan <457838219@qq.com>
2020-07-01 12:20:49 +0000

    填充用户和话题数据

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protedted $fillable = ['name', 'description'];
+    protected $fillable = ['name', 'description'];
 }
--- database/migrations/2020_06_26_110506_add_avatar_and_introduction_to_users_table.php
+++ database/migrations/2020_06_26_110506_add_avatar_and_introduction_to_users_table.php
@@ -28,8 +28,7 @@ class AddAvatarAndIntroductionToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('avatar');
-            $table->dropColumn('introduction
-                ');
+            $table->dropColumn('introduction');
         });
     }
 }
